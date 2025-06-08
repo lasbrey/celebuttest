ea01fc8327b7a764cd9d18079c3c1d9e0aaf5d0b
Lazarus lawal <67360907+lasbrey@users.noreply.github.com>
2025-06-08 08:43:13 +0100

    Updated page.tsx

--- app/(site)/profile/page.tsx
+++ app/(site)/profile/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import ProfileHeader from "@/components/profile/ProfileHeader";
+import AboutSection from "@/components/profile/AboutSection";
+import FriendsSection from "@/components/profile/FriendsSection";
+import CelebrationSection from "@/components/profile/CelebrationSection";
+import TabsSection from "@/components/profile/TabsSection";
 
-const page = () => {
+const ProfilePage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="min-h-screen bg-gray-50">
+      {/* Cover Image */}
+      <div className="h-80 bg-gradient-to-r from-amber-100 to-orange-100 relative">
+        <img
+          src="https://images.unsplash.com/photo-1533090161767-e6ffed986c88?w=1600&q=80"
+          alt="Cover"
+          className="w-full h-full object-cover"
+        />
+      </div>
 
-export default page
+      <div className="max-w-6xl mx-auto px-4">
+        {/* Profile Header */}
+        <ProfileHeader />
+        <div className="grid grid-cols-3 gap-6">
+          <div className="col-span-2">
+            <TabsSection />
+            <CelebrationSection />
+          </div>
+          <div className="grid gap-5">
+            <AboutSection />
+            <FriendsSection />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
